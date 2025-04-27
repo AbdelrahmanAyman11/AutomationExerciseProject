7e4918b8ba3c322312a5aa7738b54450e3cbef3e
AbdelrahmanAyman11 <abdelrahmanayman434@gmail.com>
2025-04-27 21:20:10 +0300

    commit

--- src/main/java/Pages/Signup.java
+++ src/main/java/Pages/Signup.java
@@ -2,18 +2,34 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class Signup {
     private WebDriver driver;
-
+    Select objSelect;
     public Signup(WebDriver driver) {
         this.driver = driver;
     }
 
     By Name = By.name("name");
-By Email= By.name("email");
+By Email= By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[3]");
 By SignUpButton=By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/button");
 
+By BoxVisbility = By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/h2");
+By Gender=By.id("id_gender1");
+By pass=By.id("password");
+
+By day=By.id("days");
+By month=By.id("months");
+By year=By.id("years");
+By FN=By.id("first_name");
+By LN=By.id("last_name");
+By address=By.id("address1");
+By Country=By.id("country");
+By State=By.id("state");
+By city=By.id("city");
+By ZipCode=By.id("zipcode");
+By Mobile=By.id("mobile_number");
 public void entername(String name)
 {
     driver.findElement(Name).sendKeys(name);
@@ -27,6 +43,20 @@ public void entername(String name)
     public void clicksignupbutton()
     {
         driver.findElement(SignUpButton).submit();
+
+    }
+     public boolean HeaderVisibilty()
+    {
+        boolean x= driver.findElement(BoxVisbility).isDisplayed();
+        return x;
+    }
+    public void gender()
+    {
+        driver.findElement(Gender).click();
+    }
+    public void enterPassword(String Password)
+    {
+        driver.findElement(pass).sendKeys("123456aa");
 
     }
 
--- src/test/java/SignupTest.java
+++ src/test/java/SignupTest.java
@@ -1,13 +1,41 @@
+import Pages.Login;
+import Pages.Signup;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class SignupTest {
 WebDriver driver;
+Signup signupObj;
 @BeforeTest
     public void setDriver(){
-    driver =new ChromeDriver();
+    driver =new EdgeDriver();
     driver.manage().window().maximize();
     driver.get("https://automationexercise.com/login");
 }
+    @Test
+    public void valid_Signup(){
+    signupObj =new Signup(driver);
+    boolean x= signupObj.HeaderVisibilty();
+
+        if (x==true) {
+            System.out.println("Header is Displayed !!");
+        }
+    signupObj.entername("Abdo");
+    signupObj.enteremail("noogafy567@gmail.com");
+    signupObj.clicksignupbutton();
+    }
+    @Test
+    public void CompleteSignUp(){
+
+    }
+    @AfterTest
+    public void quit()
+    {
+        driver.quit();
+    }
+
 }
