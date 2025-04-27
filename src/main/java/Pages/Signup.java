96a6012ee72341f3e7cece482d7aaa90cd62f956
AbdelrahmanAyman11 <abdelrahmanayman434@gmail.com>
2025-04-27 20:38:14 +0300

    commit

--- src/main/java/Pages/Signup.java
+++ src/main/java/Pages/Signup.java
@@ -1,4 +1,30 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class Signup {
+    private WebDriver driver;
+By Name = By.name("name");
+By Email= By.name("email");
+By SignUpButton=By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/button");
+
+public void entername(String name)
+{
+    driver.findElement(Name).sendKeys(name);
+
+}
+    public void enteremail(String email)
+    {
+        driver.findElement(Email).sendKeys(email);
+
+    }
+    public void clicksignupbutton()
+    {
+        driver.findElement(SignUpButton).submit();
+
+    }
+
+
+
 }
