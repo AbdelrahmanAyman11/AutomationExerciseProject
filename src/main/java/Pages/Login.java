ab3cad1596d25f5cc8fa752a3b55f3b7d66a944b
AbdelrahmanAyman11 <abdelrahmanayman434@gmail.com>
2025-06-10 00:16:46 +0300

    commit

--- src/main/java/Pages/Login.java
+++ src/main/java/Pages/Login.java
@@ -1,4 +1,32 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class Login {
+    private WebDriver driver;
+
+    public Login(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By Email = By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[2]");
+    By Password =By.xpath("/html/body/section/div/div/div[1]/div/form/input[3]");
+    By LoginButton = By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/button");
+
+    public void enterEmail(String email)
+    {
+        driver.findElement(Email).sendKeys(email);
+
+    }
+    public void enterPass(String Passw)
+    {
+        driver.findElement(Password).sendKeys(Passw);
+
+    }
+    public void ClickLogin()
+    {
+        driver.findElement(LoginButton).submit();
+    }
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,2 +1,31 @@
+import Pages.Login;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class LoginTest {
+    WebDriver driver;
+    Login login ;
+    @BeforeTest
+    public void setDriver(){
+        driver =new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://automationexercise.com/login");
+
+    }
+    @Test
+    public void Valid_LOGIN(){
+        login=new Login(driver);
+        login.enterEmail("noogafy567@gmail.com");
+        login.enterPass("123456aa");
+        login.ClickLogin();
+    }
+    @AfterTest
+    public void quit()
+    {
+        driver.quit();
+    }
+
 }
